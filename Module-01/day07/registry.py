69948dd7fc37d403d14a9161941223103d72512f
Hamere Gashaw <hamerega1@gmail.com>
2026-07-21 20:37:52 +0300

    Create Account class with transaction history stack

---
+++ Module-01/day07/registry.py
@@ -0,0 +1,6 @@
+class Account:
+    def __init__(self, owner, account_number, balance=0):
+        self.owner = owner
+        self.account_number = account_number
+        self.balance = balance
+        self.history = []  
